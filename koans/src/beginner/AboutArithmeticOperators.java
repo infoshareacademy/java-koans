b16006cfe785d671bf66dbc4cc9682cf05a22b3c
Trojniak Slawomir <sly@troyniak.com>
2018-11-20 20:56:24 +0100

    Koany rozwiazane, graddle-wrapper zmieniony by obslugiwal jdk 11

--- koans/src/beginner/AboutArithmeticOperators.java
+++ koans/src/beginner/AboutArithmeticOperators.java
@@ -9,49 +9,49 @@ public class AboutArithmeticOperators {
 
     @Koan
     public void simpleOperations() {
-        assertEquals(1, __);
-        assertEquals(1 + 1, __);
-        assertEquals(2 + 3 * 4, __);
-        assertEquals((2 + 3) * 4, __);
-        assertEquals(2 * 3 + 4, __);
-        assertEquals(2 - 3 + 4, __);
-        assertEquals(2 + 4 / 2, __);
-        assertEquals((2 + 4) / 2, __);
+        assertEquals(1, 2/2);
+        assertEquals(1 + 1, 2);
+        assertEquals(2 + 3 * 4, 7 * 2);
+        assertEquals((2 + 3) * 4, 2 * 10);
+        assertEquals(2 * 3 + 4, 2 * 5);
+        assertEquals(2 - 3 + 4, 3);
+        assertEquals(2 + 4 / 2, 8 / 2);
+        assertEquals((2 + 4) / 2, 3);
     }
 
     @Koan
     public void notSoSimpleOperations() {
-        assertEquals(1 / 2, __);
-        assertEquals(3 / 2, __);
-        assertEquals(1 % 2, __);
-        assertEquals(3 % 2, __);
+        assertEquals(1 / 2, 8 / 16);
+        assertEquals(3 / 2, 6 / 4);
+        assertEquals(1 % 2, 1);
+        assertEquals(3 % 2, 1);
     }
 
     @Koan
     public void minusMinusVariableMinusMinus() {
         int i = 1;
-        assertEquals(--i, __);
-        assertEquals(i, __);
-        assertEquals(i--, __);
-        assertEquals(i, __);
+        assertEquals(--i, 0); //pomniejsza o 1 wartosc po pomniejszeniu
+        assertEquals(i, 0);
+        assertEquals(i--, 0); //pomniejsza o 1 ale wartosc sprzed pomniejszenia
+        assertEquals(i, -1);
     }
 
     @Koan
     public void plusPlusVariablePlusPlus() {
         int i = 1;
-        assertEquals(++i, __);
-        assertEquals(i, __);
-        assertEquals(i++, __);
-        assertEquals(i, __);
+        assertEquals(++i, 2);
+        assertEquals(i, 2);
+        assertEquals(i++, 2);
+        assertEquals(i, 3);
     }
 
     @Koan
     public void timesAndDivInPlace() {
         int i = 1;
         i *= 2;
-        assertEquals(i, __);
+        assertEquals(i, 2);
         i /= 2;
-        assertEquals(i, __);
+        assertEquals(i, 1);
     }
 
 }
