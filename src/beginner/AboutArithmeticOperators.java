df4830f12f24d69d30b8346222d053c1ac7339d5
Szakalakazo <tomek.starzak@gmail.com>
2018-11-29 16:04:46 +0100

    AboutConditionasl. AboutLoops, AboutStrings, AbouthArithmeticOperaitors completed

--- src/beginner/AboutArithmeticOperators.java
+++ src/beginner/AboutArithmeticOperators.java
@@ -21,37 +21,37 @@ public class AboutArithmeticOperators {
 
     @Koan
     public void notSoSimpleOperations() {
-        assertEquals(1 / 2, __);
-        assertEquals(3 / 2, __);
-        assertEquals(1 % 2, __);
-        assertEquals(3 % 2, __);
+        assertEquals(1 / 2, 0);
+        assertEquals(3 / 2, 1);
+        assertEquals(1 % 2, 1);
+        assertEquals(3 % 2, 1);
     }
 
     @Koan
     public void minusMinusVariableMinusMinus() {
         int i = 1;
-        assertEquals(--i, __);
-        assertEquals(i, __);
-        assertEquals(i--, __);
-        assertEquals(i, __);
+        assertEquals(--i, 0);
+        assertEquals(i, 0);
+        assertEquals(i--, 0);
+        assertEquals(i, -1);
     }
 
     @Koan
     public void plusPlusVariablePlusPlus() {
         int i = 1;
-        assertEquals(++i, __);
-        assertEquals(i, __);
-        assertEquals(i++, __);
-        assertEquals(i, __);
+        assertEquals(++i, 2);
+        assertEquals(i, 2);
+        assertEquals(i++, 2);
+        assertEquals(i, 3);
     }
 
     @Koan
     public void timesAndDivInPlace() {
         int i = 1;
         i *= 2;
-        assertEquals(i, __);
+        assertEquals(i, 2);
         i /= 2;
-        assertEquals(i, __);
+        assertEquals(i, 1);
     }
 
 }
