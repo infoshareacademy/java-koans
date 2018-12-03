65f92664641a38804831d4c7bd48852c256d752f
Trojniak Slawomir <sly@troyniak.com>
2018-12-03 21:25:31 +0100

    AboutException solved but very frustrating

---
+++ temp/temp.java
@@ -0,0 +1,10 @@
+public void nestedLoops() {
+        String s = "";
+        for (int i = 0; i < 3; i++){
+        for(int j=0;j< 3;j++){
+        s+="("+i+", "+j+") ";
+        }
+        s+=" - ";
+sout
+
+        }
