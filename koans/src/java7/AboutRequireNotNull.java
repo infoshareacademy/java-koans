8c5982aefbc7dec7b2bd9b9c535e4b018a480e6d
MaciejGol90 <maciek.golianek@gmail.com>
2018-12-05 14:51:58 +0100

    KoansZaj3

--- koans/src/java7/AboutRequireNotNull.java
+++ koans/src/java7/AboutRequireNotNull.java
@@ -19,7 +19,7 @@ public class AboutRequireNotNull {
         } catch (NullPointerException ex) {
             s = "caught a NullPointerException";
         }
-        assertEquals(s, __);
+        assertEquals(s, "caught a NullPointerException");
     }
 
     @Koan
