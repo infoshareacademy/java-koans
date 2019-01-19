f208e2a7e3161bc2bbf9ac31adb8436e19bd8d70
Kamil Ganski <kamilganski2@gmail.com>
2019-01-19 18:23:30 +0100

    Finished class AboutComparison

--- koans/src/intermediate/AboutComparison.java
+++ koans/src/intermediate/AboutComparison.java
@@ -14,9 +14,9 @@ public class AboutComparison {
     public void compareObjects() {
         String a = "abc";
         String b = "bcd";
-        assertEquals(a.compareTo(b), __);
-        assertEquals(a.compareTo(a), __);
-        assertEquals(b.compareTo(a), __);
+        assertEquals(a.compareTo(b), -1);
+        assertEquals(a.compareTo(a), 0);
+        assertEquals(b.compareTo(a), 1);
     }
 
     static class Car implements Comparable<Car> {
@@ -36,7 +36,7 @@ public class AboutComparison {
         vwbeetle.horsepower = 50;
         Car porsche = new Car();
         porsche.horsepower = 300;
-        assertEquals(vwbeetle.compareTo(porsche), __);
+        assertEquals(vwbeetle.compareTo(porsche), -250);
     }
 
     static class RaceHorse {
@@ -45,18 +45,21 @@ public class AboutComparison {
 
         @Override
         public String toString() {
+
             return "Speed: " + speed + " Age: " + age;
         }
     }
 
     static class HorseSpeedComparator implements Comparator<RaceHorse> {
         public int compare(RaceHorse o1, RaceHorse o2) {
+
             return o1.speed - o2.speed;
         }
     }
 
     static class HorseAgeComparator implements Comparator<RaceHorse> {
         public int compare(RaceHorse o1, RaceHorse o2) {
+
             return o1.age - o2.age;
         }
     }
@@ -76,8 +79,8 @@ public class AboutComparison {
         RaceHorse[] horses = {lindy, slowy, lightning};
 
         Arrays.sort(horses, new HorseAgeComparator());
-        assertEquals(horses[0], __);
+        assertEquals(horses[0], lightning);
         Arrays.sort(horses, new HorseSpeedComparator());
-        assertEquals(horses[0], __);
+        assertEquals(horses[0], slowy);
     }
 }
